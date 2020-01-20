80934d14fbbe52f1ea24e5c6f9daa816413ef1c8
Victor Santiago <vsantiago113sec@gmail.com>
2020-01-19 20:10:24 -0600

    Update

--- BlueCatClient/__init__.py
+++ BlueCatClient/__init__.py
@@ -1,7 +1,6 @@
 import requests
 import urllib3
 import re
-from urllib.parse import urlencode, quote_plus
 
 
 class BlueCatAuthError(Exception):
@@ -13,34 +12,24 @@ class BlueCatError(Exception):
 
 
 class Client:
-    def __init__(self, verify=False, warnings=False, api_version='v1', proxy_http=None, proxy_https=None):
-        if verify is False:
-            self.verify = verify
-        elif verify is True:
-            self.verify = verify
-            
-        if warnings is False:
-            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        elif warnings is True:
-            pass
+    def __init__(self, verify=bool(), warnings=bool(), api_version='v1'):
+        self.verify = bool(verify)
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) if warnings is False else None
 
         self.api_version = api_version
-        self.headers = {'Content-Type': 'application/json'}
-        self.proxies = {'http': proxy_http, 'https': proxy_https}
         self.base_url = None
         self.token = None
         self.session = None
-        
+
     def connect(self, server=None, username=None, password=None):
         server = server.replace('https://', '').replace('http://', '').rstrip('/')
-        self.base_url = 'https://{}/Services/REST/{}/'.format(server.rstrip('/'), self.api_version)
+        self.base_url = f'https://{server.rstrip("/")}/Services/REST/{self.api_version}'
         session = requests.Session()
-        session.headers.update(self.headers)
-        session.proxies = self.proxies
+        session.headers.update({'Content-Type': 'application/json'})
         session.verify = self.verify
-        response = session.get(self.base_url + 'login', params={'username': username,
-                                                                'password': password})
-        token = re.search(r'->\s(\w+:\s\w+==)\s<-', response.text, flags=re.I)
+        response = session.get(self.base_url + '/login', params={'username': username,
+                                                                 'password': password})
+        token = re.search(r'->\s(\w+:\s\w+(?:==)?)\s<-', response.text, flags=re.I)
         if token:
             token = token.group(1)
             session.headers.update({'Authorization': token})
@@ -49,22 +38,42 @@ class Client:
         else:
             raise BlueCatAuthError('Authentication Error!')
 
-    def close(self):
-        response = self.session.get(self.base_url + 'logout')
+    def close(self) -> int:
+        response = self.session.get(self.base_url + '/logout')
         return response.status_code
 
     @staticmethod
-    def properties_to_json(properties):
+    def properties_to_json(properties: str) -> dict:
         return {i[0]: i[1] for i in [i.split('=') for i in properties.split('|') if i]}
 
     @staticmethod
-    def json_to_properties(properties):
-        return {'properties': '|'.join(['{}={}'.format(k, v) for k, v in properties.items()])}
+    def json_to_properties(properties: dict) -> dict:
+        return {'properties': '|'.join([f'{k}={v}' for k, v in properties.items()])}
 
-    def get(self, method=None, **kwargs):
-        kwargs = '?' + urlencode(kwargs, quote_via=quote_plus) if urlencode(kwargs, quote_via=quote_plus) else ''
-        response = self.session.get('{}{}{}'.format(self.base_url, method, kwargs))
+    def get(self, method=None, **kwargs) -> dict:
+        response = self.session.get(f'{self.base_url}/{method}', params=kwargs)
         if response.status_code in [200]:
             return response.json()
         else:
-            raise BlueCatError(response.status_code)
+            raise BlueCatError(response.text, response.status_code)
+
+    def add(self, method=None, **kwargs):
+        response = self.session.post(f'{self.base_url}/{method}', params=kwargs)
+        if response.status_code in [200]:
+            return 'The Entity was created successfully!'
+        else:
+            raise BlueCatError(response.text, response.status_code)
+
+    def update(self, entity: dict) -> str:
+        response = self.session.put(f'{self.base_url}/update', json=entity)
+        if response.status_code in [200]:
+            return 'The Entity was updated successfully!'
+        else:
+            raise BlueCatError(response.text, response.status_code)
+
+    def delete(self, **kwargs) -> str:
+        response = self.session.delete(f'{self.base_url}/delete', params=kwargs)
+        if response.status_code in [200]:
+            return 'The Entity was deleted successfully!'
+        else:
+            raise BlueCatError(response.text, response.status_code)
